1c4bafe96cf9aa707e10d5725625f45b9cb5cea3
Marivi Pelaez Alonso <mariavictoria.pelaezalonso@telefonica.com>
2016-05-12 13:40:42 +0200

    Add a cache to manage generated images

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,8 +1,10 @@
 from flask import Flask, Response, request
 import requests
 import hashlib
+import redis
 
 app = Flask(__name__)
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
 
 default_name = 'Joe Bloggs'
 salt = "UNIQUE_SALT"
@@ -36,8 +38,12 @@ def mainpage():
 @app.route('/monster/<name>')
 def get_identicon(name):
 
-    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
-    image = r.content
+    image = cache.get(name)
+    if not image:
+        print("Image was not cached", flush=True)
+        r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+        image = r.content
+        cache.set(name, image)
 
     return Response(image, mimetype='image/png')
 
